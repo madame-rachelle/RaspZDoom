d47392c8240ba98ca987265ba133333a6828944a
Christoph Oelckers <coelckers@zdoom.fake>
2014-05-11 13:29:06 +0200

    - fixed: the delayed state changer for the alpha function called glBlendFunc instead of glAlphaFunc.
    (cherry picked from commit cd6d653b59e8821f376e8aca6929f8091ff032cd)

--- src/gl/renderer/gl_renderstate.cpp
+++ src/gl/renderer/gl_renderstate.cpp
@@ -271,7 +271,7 @@ void FRenderState::Apply(bool forcenoshader)
 		{
 			glAlphaFunc = mAlphaFunc;
 			glAlphaThreshold = mAlphaThreshold;
-			::glBlendFunc(mAlphaFunc, mAlphaThreshold);
+			::glAlphaFunc(mAlphaFunc, mAlphaThreshold);
 		}
 		if (mAlphaTest != glAlphaTest)
 		{
