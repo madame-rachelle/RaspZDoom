1e56f3ae2f347a77bacfd93b96352a777568bb83
Christoph Oelckers <coelckers@zdoom.fake>
2014-09-21 11:08:38 +0200

    - fixed weapon drawing rules to account for the death camera.
    (cherry picked from commit 7359631a43274e3c8850b0a53a9a237aaa970b9c)

--- src/gl/scene/gl_weapon.cpp
+++ src/gl/scene/gl_weapon.cpp
@@ -60,6 +60,7 @@ EXTERN_CVAR (Bool, r_drawplayersprites)
 EXTERN_CVAR(Float, transsouls)
 EXTERN_CVAR (Bool, st_scale)
 EXTERN_CVAR(Int, gl_fuzztype)
+EXTERN_CVAR (Bool, r_deathcamera)
 
 
 //==========================================================================
@@ -188,7 +189,8 @@ void FGLRenderer::DrawPlayerSprites(sector_t * viewsector, bool hudModelStep)
 	if (!player ||
 		!r_drawplayersprites ||
 		!camera->player ||
-		(players[consoleplayer].cheats & CF_CHASECAM))
+		(player->cheats & CF_CHASECAM) || 
+		(r_deathcamera && camera->health <= 0))
 		return;
 
 	/*
