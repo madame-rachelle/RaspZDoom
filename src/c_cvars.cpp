b178d956b5eadfda6326cab8ba7ba10c9a49dc5b
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-01-31 11:05:29 +0200

    Added cheat flag for console variables
    CVAR with this flag can be set in console or from command line when sv_cheats is enabled
    There is no such restriction for changing its value from ACS, via SetCVar() and related functions
    'cheat' modifier can be used in CVARINFO lump to create variable of this kind
    
    (cherry picked from commit 6225f60eb210fcdf3341b7233854a6715b241ac0)
    
    # Conflicts:
    #	src/c_cvars.cpp
    
    - Fixed bad merge of "Fixed resetting CVARs via internal menu CCMDs"

--- src/c_cvars.cpp
+++ src/c_cvars.cpp
@@ -1561,6 +1561,8 @@ void C_ArchiveCVars (FConfigFile *f, uint32 filter)
 	}
 }
 
+EXTERN_CVAR(Bool, sv_cheats);
+
 static bool IsUnsafe(const FBaseCVar *const var)
 {
 	const bool unsafe = UnsafeExecutionContext && !(var->GetFlags() & CVAR_MOD);
@@ -1573,6 +1575,16 @@ static bool IsUnsafe(const FBaseCVar *const var)
 
 void FBaseCVar::CmdSet (const char *newval)
 {
+	if ((GetFlags() & CVAR_CHEAT) && !sv_cheats)
+	{
+		Printf("sv_cheats must be true to set this console variable.\n");
+		return;
+	}
+	else if (IsUnsafe(this))
+	{
+		return;
+	}
+
 	UCVarValue val;
 
 	// Casting away the const is safe in this case.
