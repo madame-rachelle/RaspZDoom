367df7eef35d04b3a1971d5b1ff74a3212fed06b
Christoph Oelckers <coelckers@users.noreply.github.com>
2017-03-29 01:25:02 +0200

    - added Floor/Ceiling_MoveToValueAndCrush action specials.
    (cherry picked from commit 914c829f798fffa2b0ea25e5a061936073ae9966)
    
    # Conflicts:
    #	src/p_lnspec.cpp

--- src/p_lnspec.cpp
+++ src/p_lnspec.cpp
@@ -454,6 +454,13 @@ FUNC(LS_Floor_MoveToValue)
 					   arg2*(arg3?-1:1), -1, CHANGE(arg4), false);
 }
 
+FUNC(LS_Floor_MoveToValueAndCrush)
+// Floor_MoveToValueAndCrush (tag, speed, height, crush, crushmode)
+{
+	return EV_DoFloor(DFloor::floorMoveToValue, ln, arg0, SPEED(arg1),
+		arg2, CRUSH(arg3) -1, 0, CRUSHTYPE(arg4), false);
+}
+
 FUNC(LS_Floor_RaiseToLowestCeiling)
 // Floor_RaiseToLowestCeiling (tag, speed, change, crush)
 {
@@ -747,6 +754,13 @@ FUNC(LS_Ceiling_MoveToValue)
 						 arg2*((arg3) ? -1 : 1), -1, 0, CHANGE(arg4));
 }
 
+FUNC(LS_Ceiling_MoveToValueAndCrush)
+// Ceiling_MoveToValueAndCrush (tag, speed, height, crush, crushmode)
+{
+	return EV_DoCeiling (DCeiling::ceilMoveToValue, ln, arg0, SPEED(arg1), 0,
+						 arg2, CRUSH(arg3), 0, 0, CRUSHTYPE(arg4, arg1 == 8));
+}
+
 FUNC(LS_Ceiling_LowerToHighestFloor)
 // Ceiling_LowerToHighestFloor (tag, speed, change, crush, gap)
 {
@@ -858,13 +872,13 @@ FUNC(LS_Ceiling_ToFloorInstant)
 FUNC(LS_Ceiling_LowerToFloor)
 // Ceiling_LowerToFloor (tag, speed, change, crush, gap)
 {
-	return EV_DoCeiling (DCeiling::ceilLowerToFloor, ln, arg0, SPEED(arg1), 0, arg4, CRUSH(arg3), 0, CHANGE(arg4));
+	return EV_DoCeiling (DCeiling::ceilLowerToFloor, ln, arg0, SPEED(arg1), 0, arg4, CRUSH(arg3), 0, CHANGE(arg2));
 }
 
 FUNC(LS_Ceiling_LowerByTexture)
 // Ceiling_LowerByTexture (tag, speed, change, crush)
 {
-	return EV_DoCeiling (DCeiling::ceilLowerByTexture, ln, arg0, SPEED(arg1), 0, 0, CRUSH(arg3), 0, CHANGE(arg4));
+	return EV_DoCeiling (DCeiling::ceilLowerByTexture, ln, arg0, SPEED(arg1), 0, 0, CRUSH(arg3), 0, CHANGE(arg2));
 }
 
 FUNC(LS_Ceiling_Stop)
@@ -3624,6 +3638,8 @@ static lnSpecFunc LineSpecials[] =
 	/* 274 */ LS_Door_AnimatedClose,
 	/* 275 */ LS_Floor_Stop,
 	/* 276 */ LS_Ceiling_Stop,
+	/* 279 */ LS_Floor_MoveToValueAndCrush,
+	/* 280 */ LS_Ceiling_MoveToValueAndCrush,
 
 };
 
