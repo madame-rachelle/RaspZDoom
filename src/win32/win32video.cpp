3c13473ec970c0e06f3cdcaa6652bdbf29e66f7e
Rachael Alexanderson <eruanna@drdteam.org>
2017-05-29 01:01:04 -0400

    - Print out warnings when Direct3D initialization fails.
    (cherry picked from commit b7295775b9cd734e8011df784e982b829568680e)

--- src/win32/win32video.cpp
+++ src/win32/win32video.cpp
@@ -200,6 +200,7 @@ bool Win32Video::InitD3D9 ()
 	// Load the Direct3D 9 library.
 	if ((D3D9_dll = LoadLibraryA ("d3d9.dll")) == NULL)
 	{
+		Printf("Unable to load d3d9.dll! Falling back to DirectDraw...\n");
 		return false;
 	}
 
@@ -261,6 +262,7 @@ d3drelease:
 	D3D = NULL;
 closelib:
 	FreeLibrary (D3D9_dll);
+	Printf("Direct3D acceleration failed! Falling back to DirectDraw...\n");
 	return false;
 }
 
