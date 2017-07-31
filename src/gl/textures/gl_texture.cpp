de741e46d525d0ce99570f016ef7f6d2d080d277
drfrag <drfrag666@hotmail.com>
2017-07-31 18:42:29 +0200

    - Changed some defaults for textures and sprites.

--- src/gl/textures/gl_texture.cpp
+++ src/gl/textures/gl_texture.cpp
@@ -58,7 +58,7 @@
 // Texture CVARs
 //
 //==========================================================================
-CUSTOM_CVAR(Float,gl_texture_filter_anisotropic,8.0f,CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINITCALL)
+CUSTOM_CVAR(Float,gl_texture_filter_anisotropic,4.0f,CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINITCALL)
 {
 	if (GLRenderer != NULL) GLRenderer->FlushTextures();
 }
@@ -68,7 +68,7 @@ CCMD(gl_flush)
 	if (GLRenderer != NULL) GLRenderer->FlushTextures();
 }
 
-CUSTOM_CVAR(Int, gl_texture_filter, 4, CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINITCALL)
+CUSTOM_CVAR(Int, gl_texture_filter, 0, CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINITCALL)
 {
 	if (self < 0 || self > 5) self=4;
 	if (GLRenderer != NULL) GLRenderer->FlushTextures();
