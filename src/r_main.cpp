5d6a22e8c3c4c38c24fbd4c3e44e9e320ecc6a77
drfrag <drfrag666@hotmail.com>
2017-10-04 13:52:28 +0200

    - Added new quad horizontally and vertically low detail mode.
    # Conflicts:
    #	src/r_main.cpp

--- src/r_main.cpp
+++ src/r_main.cpp
@@ -313,12 +313,12 @@ CUSTOM_CVAR (Int, r_detail, 0, CVAR_ARCHIVE|CVAR_GLOBALCONFIG)
 		return;
 	}
 
-	if (self < 0 || self > 5 || r_truecolor)
+	if (self < 0 || self > 6 || r_truecolor)
 	{
 		badrecovery = true;
 		if (!r_truecolor)
 		{
-			Printf ("Bad detail mode. (Use 0-5)\n");
+			Printf ("Bad detail mode. (Use 0-6)\n");
 			self = (detailyshift << 1) | detailxshift;
 		}
 		else
