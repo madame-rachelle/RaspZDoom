28d433e1b08ca95124d5edda38858f7409072e42
drfrag <drfrag666@hotmail.com>
2017-10-05 10:21:48 +0200

    - Adjustment for reduced screen sizes in new low detail modes.

--- src/r_draw.cpp
+++ src/r_draw.cpp
@@ -208,6 +208,7 @@ FDynamicColormap identitycolormap;
 
 EXTERN_CVAR (Int, r_columnmethod)
 EXTERN_CVAR (Int, r_detail)
+EXTERN_CVAR (Int, screenblocks)
 
 void R_InitShadeMaps()
 {
@@ -2781,8 +2782,12 @@ void R_DetailDouble ()
 			int y,x;
 			BYTE *linefrom, *lineto;
 			BYTE c;
-			int offset = viewwidth > 320 ? CPU.DataL1LineSize : 0;
+			int offset = SCREENWIDTH > 640 ? CPU.DataL1LineSize : 0;
 
+			if (screenblocks <= 9)
+			{
+				offset = offset + (10-screenblocks)*(SCREENWIDTH/10) + SCREENWIDTH%10;
+			}
 			linefrom = dc_destorg;
 			lineto = linefrom - viewwidth;
 			for (y = 0; y < viewheight; ++y)
@@ -2823,8 +2828,12 @@ void R_DetailDouble ()
 			int y,x;
 			BYTE *linefrom, *lineto;
 			BYTE c;
-			int offset = viewwidth > 320 ? CPU.DataL1LineSize*2 : 0;
+			int offset = SCREENWIDTH > 640 ? CPU.DataL1LineSize*2 : 0;
 
+			if (screenblocks <= 9)
+			{
+				offset = offset + 2*(10-screenblocks)*(SCREENWIDTH/10) + 2*(SCREENWIDTH%10);
+			}
 			linefrom = dc_destorg;
 			lineto = linefrom - viewwidth;
 			for (y = 0; y < viewheight/2; ++y)
