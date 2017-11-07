42ee9271ae61999478fc989e1c659e5472b39fe2
drfrag <drfrag666@hotmail.com>
2017-11-07 09:47:49 +0100

    - Changed version numbering scheme. New version.
    # Conflicts:
    #	src/version.h

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "2.8.2"
+#define VERSIONSTR "2.8.2a"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,8,2,0
-#define RC_PRODUCTVERSION 2,8,2,0
-#define RC_PRODUCTVERSION2 "2.8.2"
+#define RC_FILEVERSION 2,8,2,1
+#define RC_PRODUCTVERSION 2,8,2,1
+#define RC_PRODUCTVERSION2 "2.8.2a"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
