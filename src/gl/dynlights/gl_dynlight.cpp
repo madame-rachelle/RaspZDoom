d3ac0b996d6f69c17202031bb175cdbf54d4f55d
Christoph Oelckers <coelckers@users.noreply.github.com>
2017-11-19 15:30:45 +0100

    - fixed bad use of FScanner::GetNumber and GetFloat in code inherited from ZDoomGL.
    This could lead to bad error messages if some malformed definitions were used.
    
    (cherry picked from commit f0dc619b5b3e45739858e83592a85dc0f0022160)

--- src/gl/dynlights/gl_dynlight.cpp
+++ src/gl/dynlights/gl_dynlight.cpp
@@ -243,7 +243,7 @@ extern int ScriptDepth;
 
 inline float gl_ParseFloat(FScanner &sc)
 {
-   sc.GetFloat();
+   sc.MustGetFloat();
 
    return float(sc.Float);
 }
@@ -251,7 +251,7 @@ inline float gl_ParseFloat(FScanner &sc)
 
 inline int gl_ParseInt(FScanner &sc)
 {
-   sc.GetNumber();
+   sc.MustGetNumber();
 
    return sc.Number;
 }
