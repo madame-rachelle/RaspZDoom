283fc66cc589fda4f2f66737678b88560c0ac6aa
Rachael Alexanderson <raa-eruanna@users.noreply.github.com>
2017-12-07 06:32:51 -0500

    - implemented win_borderless for fake fullscreen in Windows
    (cherry picked from commit 938cd3cabea6f62ec2c596e87df33080692c25a6)
    
    # Conflicts:
    #	src/win32/hardware.cpp

--- src/win32/hardware.cpp
+++ src/win32/hardware.cpp
@@ -53,6 +53,7 @@ EXTERN_CVAR (Bool, ticker)
 EXTERN_CVAR (Bool, fullscreen)
 EXTERN_CVAR (Bool, r_truecolor)
 EXTERN_CVAR (Float, vid_winscale)
+EXTERN_CVAR (Bool, win_borderless)
 
 EXTERN_CVAR(Int, r_skymode)
 EXTERN_CVAR(Int, r_detail)
@@ -316,6 +317,13 @@ void I_RestoreWindowedPos ()
 	}
 	MoveWindow (Window, winx, winy, winw, winh, TRUE);
 
+	if (win_borderless && !Args->CheckParm("-0"))
+	{
+		LONG lStyle = GetWindowLong(Window, GWL_STYLE);
+		lStyle &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZE | WS_MAXIMIZE | WS_SYSMENU);
+		SetWindowLong(Window, GWL_STYLE, lStyle);
+		SetWindowPos(Window, HWND_TOP, 0, 0, scrwidth, scrheight, 0);
+	}
 	if (win_maximized && !Args->CheckParm("-0"))
 		ShowWindow(Window, SW_MAXIMIZE);
 }
@@ -332,6 +340,18 @@ CUSTOM_CVAR(Bool, r_truecolor, false, CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINIT
 	NewHeight = screen->GetHeight();
 	NewBits = DisplayBits;
 	setmodeneeded = true;
+}
+
+CUSTOM_CVAR(Bool, win_borderless, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG | CVAR_NOINITCALL)
+{
+	// Just reinit the window. Saves a lot of trouble.
+	if (!fullscreen)
+	{
+		NewWidth = screen->GetWidth();
+		NewHeight = screen->GetHeight();
+		NewBits = DisplayBits;
+		setmodeneeded = true;
+	}
 }
 
 CUSTOM_CVAR (Bool, fullscreen, true, CVAR_ARCHIVE|CVAR_GLOBALCONFIG|CVAR_NOINITCALL)
