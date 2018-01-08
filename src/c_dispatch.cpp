e7b3dee4160ba389568e4f10be724a95a8134fab
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-01-08 18:13:02 +0100

    - DWaitingCommand needs to serialize its unsafe state.
    (cherry picked from commit 8667987cd65e709b8eb5c02e0b603d7aa515c3a5)

--- src/c_dispatch.cpp
+++ src/c_dispatch.cpp
@@ -195,7 +195,8 @@ void DWaitingCommand::Serialize(FSerializer &arc)
 {
 	Super::Serialize (arc);
 	arc("command", Command)
-		("ticsleft", TicsLeft);
+		("ticsleft", TicsLeft)
+		("unsafe", IsUnsafe);
 }
 
 DWaitingCommand::DWaitingCommand ()
