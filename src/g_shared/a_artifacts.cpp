ZDoom/gzdoom

- fixed: The player checking code after loading a savegame was not changed when tracer was replaced with alternative for holding the player's unmorphed version.

src/g_inventory/a_artifacts.cpp
@@ -1893,7 +1893,7 @@ void APowerMorph::InitEffect( )
 	if (Owner != nullptr && Owner->player != nullptr && PlayerClass != nullptr)
 	{
 		player_t *realplayer = Owner->player;	// Remember the identity of the player
-		if (P_MorphPlayer(realplayer, realplayer, PlayerClass, -1/*INDEFINITELY*/, MorphStyle, MorphFlash, UnMorphFlash))
+		if (P_MorphPlayer(realplayer, realplayer, PlayerClass, INT_MAX/*INDEFINITELY*/, MorphStyle, MorphFlash, UnMorphFlash))
 		{
 			Owner = realplayer->mo;				// Replace the new owner in our owner; safe because we are not attached to anything yet
 			ItemFlags |= IF_CREATECOPYMOVED;	// Let the caller know the "real" owner has changed (to the morphed actor)

madame-rachelle/RaspZDoom

(cherry picked from commit 80f166193180beb0a8a8961dc14dca52a51d00b2)
# Conflicts:
#	src/g_shared/a_artifacts.cpp

src/g_shared/a_artifacts.cpp
@@ -1933,7 +1933,7 @@ void APowerMorph::InitEffect( )
 		PClassActor *morph_flash = PClass::FindActor(MorphFlash);
 		PClassActor *unmorph_flash = PClass::FindActor(UnMorphFlash);
 		PClassPlayerPawn *player_class = dyn_cast<PClassPlayerPawn>(PClass::FindClass (PlayerClass));
-		if (P_MorphPlayer(realplayer, realplayer, player_class, -1/*INDEFINITELY*/, MorphStyle, morph_flash, unmorph_flash))
+		if (P_MorphPlayer(realplayer, realplayer, player_class, INT_MAX/*INDEFINITELY*/, MorphStyle, morph_flash, unmorph_flash))
 		{
 			Owner = realplayer->mo;				// Replace the new owner in our owner; safe because we are not attached to anything yet
 			ItemFlags |= IF_CREATECOPYMOVED;	// Let the caller know the "real" owner has changed (to the morphed actor)