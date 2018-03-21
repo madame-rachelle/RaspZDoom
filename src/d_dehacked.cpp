68f7f6091a94ba19f248a878ce2a7be906db62d0
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-03-21 13:03:44 +0200

    Fixed crash on loading malformed DeHackEd patches
    Lump that contains only "Patch File for DeHackEd v3.0" (without line break at the end) caused a crash
    
    (cherry picked from commit 92f10febe86d0bb39de53711c5926a4609848b7e)

--- src/d_dehacked.cpp
+++ src/d_dehacked.cpp
@@ -566,6 +566,8 @@ static void stripwhite (char *str)
 
 static char *igets (void)
 {
+	assert(PatchPt != nullptr);
+
 	char *line;
 
 	if (*PatchPt == '\0' || PatchPt >= PatchFile + PatchSize )
@@ -2534,7 +2536,7 @@ static bool DoDehPatch()
 		}
 
 		PatchPt = strchr (PatchFile, '\n');
-		while ((cont = GetLine()) == 1)
+		while (PatchPt != nullptr && (cont = GetLine()) == 1)
 		{
 				 CHECKKEY ("Doom version", dversion)
 			else CHECKKEY ("Patch format", pversion)
