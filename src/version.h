57f50a0f81a069fa94167cac3da4976291a90a08
drfrag <drfrag666@hotmail.com>
2018-04-10 21:25:46 +0200

    Revert "- New version."
    This reverts commit c46f112820af0c68aada52f90a38a0a265fa6772.

--- src/version.h
+++ src/version.h
@@ -41,13 +41,13 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "2.8.2a"
+#define VERSIONSTR "2.8.2"
 #define GLVERSIONSTR "1.8.4b"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,8,2,1
-#define RC_PRODUCTVERSION 2,8,2,1
-#define RC_PRODUCTVERSION2 "2.8.2a"
+#define RC_FILEVERSION 2,8,2,0
+#define RC_PRODUCTVERSION 2,8,2,0
+#define RC_PRODUCTVERSION2 "2.8.2"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
