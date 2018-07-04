9b6ea608eeaf97e35c0baad9b36f1f1ab81b4289
drfrag666 <drfrag666@hotmail.com>
2018-07-04 21:08:14 +0200

    - Fixed potential crash on usage of Mystic Ambit Incant. (patch by _mental_)

--- src/g_hexen/a_healingradius.cpp
+++ src/g_hexen/a_healingradius.cpp
@@ -39,8 +39,12 @@ bool AArtiHealingRadius::Use (bool pickup)
 
 	for (int i = 0; i < MAXPLAYERS; ++i)
 	{
-		if (playeringame[i] &&
-			players[i].mo != NULL &&
+		if (!playeringame[i])
+		{
+			continue;
+		}
+
+		if (players[i].mo != NULL &&
 			players[i].mo->health > 0 &&
 			players[i].mo->Distance2D (Owner) <= HEAL_RADIUS_DIST)
 		{
