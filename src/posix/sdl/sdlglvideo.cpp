ZDoom/gzdoom

- moved all system specific code out of gl_interface.cpp into the respective Video classes (Win32GLVideo andSDLGLVideo.) (SDL side not tested yet!)

src/sdl/sdlglvideo.cpp
@@ -279,6 +279,67 @@ bool SDLGLVideo::SetResolution (int width, int height, int bits)
 	return true;	// We must return true because the old video context no longer exists.
 }
 
+//==========================================================================
+//
+// 
+//
+//==========================================================================
+
+bool SDLGLVideo::SetupPixelFormat(bool allowsoftware, bool nostencil, int multisample)
+{
+	int stencil;
+	
+	if (!nostencil)
+	{
+		stencil=1;
+		SDL_GL_SetAttribute( SDL_GL_RED_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,  24 );
+		SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE,  8 );
+//		SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,  1 );
+		if (multisample > 0) {
+			SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1 );
+			SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, multisample );
+		}
+	}
+	else
+	{
+		// Use the cheapest mode available and let's hope the driver can handle this...
+		stencil=0;
+
+		SDL_GL_SetAttribute( SDL_GL_RED_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,  16 );
+		//SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,  1 )*/
+	}
+	if (stencil==0)
+	{
+		gl.flags|=RFL_NOSTENCIL;
+	}
+	return true;
+}
+
+//==========================================================================
+//
+// 
+//
+//==========================================================================
+
+bool SDLGLVideo::InitHardware (bool allowsoftware, bool nostencil, int multisample)
+{
+	if (!SetupPixelFormat(allowsoftware, nostencil, multisample))
+	{
+		Printf ("R_OPENGL: Reverting to software mode...\n");
+		return false;
+	}
+	return true;
+}
+
+
 // FrameBuffer implementation -----------------------------------------------
 
 SDLGLFB::SDLGLFB (void *, int width, int height, int, int, bool fullscreen)
@@ -294,7 +355,7 @@ SDLGLFB::SDLGLFB (void *, int width, int height, int, int, bool fullscreen)
 
 	UpdatePending = false;
 	
-	if (!gl.InitHardware(false, gl_vid_compatibility, localmultisample))
+	if (!static_cast<SDLGLVideo*>(Video)->InitHardware(false, gl_vid_compatibility, localmultisample))
 	{
 		vid_renderer = 0;
 		return;
@@ -334,6 +395,9 @@ SDLGLFB::~SDLGLFB ()
 		SDL_SetGammaRamp(m_origGamma[0], m_origGamma[1], m_origGamma[2]);
 	}
 }
+
+
+
 
 void SDLGLFB::InitializeState() 
 {
@@ -415,5 +479,10 @@ void SDLGLFB::SetVSync( bool vsync )
 
 void SDLGLFB::NewRefreshRate ()
 {
+}
+
+void SDLGLFB::SwapBuffers()
+{
+	SDL_GL_SwapBuffers ();
 }
 

madame-rachelle/RaspZDoom

(cherry picked from commit 761ab4ab78cae79715a17c1cd0958735556ac2fb)
# Conflicts:
#	src/gl/system/gl_interface.cpp
#	src/win32/hardware.cpp

src/posix/sdl/sdlglvideo.cpp
@@ -26,6 +26,7 @@
 #include "gl/utility/gl_templates.h"
 #include "gl/textures/gl_material.h"
 #include "gl/system/gl_cvars.h"
+#include "doomerrors.h"
 
 // MACROS ------------------------------------------------------------------
 
@@ -279,6 +280,68 @@ bool SDLGLVideo::SetResolution (int width, int height, int bits)
 	return true;	// We must return true because the old video context no longer exists.
 }
 
+//==========================================================================
+//
+// 
+//
+//==========================================================================
+
+bool SDLGLVideo::SetupPixelFormat(bool allowsoftware, bool nostencil, int multisample)
+{
+	int stencil;
+	
+	if (!nostencil)
+	{
+		stencil=1;
+		SDL_GL_SetAttribute( SDL_GL_RED_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE,  8 );
+		SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,  24 );
+		SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE,  8 );
+//		SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,  1 );
+		if (multisample > 0) {
+			SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1 );
+			SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, multisample );
+		}
+	}
+	else
+	{
+		// Use the cheapest mode available and let's hope the driver can handle this...
+		stencil=0;
+
+		SDL_GL_SetAttribute( SDL_GL_RED_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE,  4 );
+		SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,  16 );
+		//SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,  1 )*/
+	}
+	if (stencil==0)
+	{
+		gl.flags|=RFL_NOSTENCIL;
+	}
+	return true;
+}
+
+//==========================================================================
+//
+// 
+//
+//==========================================================================
+
+bool SDLGLVideo::InitHardware (bool allowsoftware, bool nostencil, int multisample)
+{
+	if (!SetupPixelFormat(allowsoftware, nostencil, multisample))
+	{
+		vid_renderer = 0;
+		I_Error ("R_OPENGL: Reverting to software mode...\n");
+		return false;
+	}
+	return true;
+}
+
+
 // FrameBuffer implementation -----------------------------------------------
 
 SDLGLFB::SDLGLFB (void *, int width, int height, int, int, bool fullscreen)
@@ -294,7 +357,7 @@ SDLGLFB::SDLGLFB (void *, int width, int height, int, int, bool fullscreen)
 
 	UpdatePending = false;
 	
-	if (!gl.InitHardware(false, gl_vid_compatibility, localmultisample))
+	if (!static_cast<SDLGLVideo*>(Video)->InitHardware(false, gl_vid_compatibility, localmultisample))
 	{
 		vid_renderer = 0;
 		return;
@@ -334,6 +397,9 @@ SDLGLFB::~SDLGLFB ()
 		SDL_SetGammaRamp(m_origGamma[0], m_origGamma[1], m_origGamma[2]);
 	}
 }
+
+
+
 
 void SDLGLFB::InitializeState() 
 {
@@ -415,5 +481,10 @@ void SDLGLFB::SetVSync( bool vsync )
 
 void SDLGLFB::NewRefreshRate ()
 {
+}
+
+void SDLGLFB::SwapBuffers()
+{
+	SDL_GL_SwapBuffers ();
 }
 