a1956811eafb46eeac782500490683986dffdd7f
drfrag666 <drfrag666@hotmail.com>
2018-09-10 20:28:28 +0200

    - Version 2.8.5a.

--- src/version.h
+++ src/version.h
@@ -41,13 +41,13 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "2.8.5"
-#define GLVERSIONSTR "1.9.2"
+#define VERSIONSTR "2.8.5a"
+#define GLVERSIONSTR "1.9.3"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 2,8,5,0
-#define RC_PRODUCTVERSION 2,8,5,0
-#define RC_PRODUCTVERSION2 "2.8.5"
+#define RC_FILEVERSION 2,8,5,1
+#define RC_PRODUCTVERSION 2,8,5,1
+#define RC_PRODUCTVERSION2 "2.8.5a"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
