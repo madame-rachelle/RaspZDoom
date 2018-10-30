3b3bc05532709c34552f50b270ed5c85d2ebfb57
drfrag666 <drfrag666@hotmail.com>
2018-10-30 12:57:12 +0100

    - Doubled initial size for the light buffer. (patch by Graf)

--- src/gl/dynlights/gl_lightbuffer.cpp
+++ src/gl/dynlights/gl_lightbuffer.cpp
@@ -32,7 +32,7 @@
 #include "hwrenderer/dynlights/hw_dynlightdata.h"
 #include "hwrenderer/data/shaderuniforms.h"
 
-static const int INITIAL_BUFFER_SIZE = 160000;	// This means 80000 lights per frame and 160000*16 bytes == 2.56 MB.
+static const int INITIAL_BUFFER_SIZE = 320000;	// This means 80000 lights per frame and 320000*16 bytes == 5.12 MB.
 
 FLightBuffer::FLightBuffer()
 {
