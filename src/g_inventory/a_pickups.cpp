9395975611235b4f43d04645a84f00e1e7332c95
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-11-24 22:03:56 +0100

    - took the weapon selection logic out of the WeaponSlots data and blocked all direct access to the weapon slots internals
    This seriously needs to be independent from the data store and better abstracted. More work to come to move this to its proper place.
    
    # Conflicts:
    #	src/g_inventory/a_pickups.cpp

--- src/g_inventory/a_pickups.cpp
+++ src/g_inventory/a_pickups.cpp
@@ -175,26 +175,6 @@ void AInventory::MarkPrecacheSounds() const
 
 //===========================================================================
 //
-// AInventory :: Use
-//
-//===========================================================================
-
-bool AInventory::CallUse(bool pickup)
-{
-	IFVIRTUAL(AInventory, Use)
-	{
-		VMValue params[2] = { (DObject*)this, pickup };
-		int retval;
-		VMReturn ret(&retval);
-		VMCall(func, params, 2, &ret, 1);
-		return !!retval;
-	}
-	return false;
-}
-
-
-//===========================================================================
-//
 //
 //===========================================================================
 static int StaticLastMessageTic;
@@ -275,25 +255,6 @@ PalEntry AInventory::CallGetBlend()
 		return retval;
 	}
 	else return 0;
-}
-
-//===========================================================================
-//
-// AInventory :: PrevItem
-//
-// Returns the previous item.
-//
-//===========================================================================
-
-AInventory *AInventory::PrevItem ()
-{
-	AInventory *item = Owner->Inventory;
-
-	while (item != NULL && item->Inventory != this)
-	{
-		item = item->Inventory;
-	}
-	return item;
 }
 
 //===========================================================================
