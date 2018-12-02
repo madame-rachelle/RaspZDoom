a8ffd3bfe692a0c173dbbea21f840cccd153ff8e
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-12-02 21:35:04 +0100

    - started replacing direct references to class AInventory.
    The easiest part was the type checks which could be changed to the name variant with a global search and replace.
    
    # Conflicts:
    #	src/hwrenderer/scene/hw_sprites.cpp

--- src/hwrenderer/scene/hw_sprites.cpp
+++ src/hwrenderer/scene/hw_sprites.cpp
@@ -289,7 +289,7 @@ void GLSprite::SplitSprite(HWDrawInfo *di, sector_t * frontsector, bool transluc
 void GLSprite::PerformSpriteClipAdjustment(AActor *thing, const DVector2 &thingpos, float spriteheight)
 {
 	const float NO_VAL = 100000000.0f;
-	bool clipthing = (thing->player || thing->flags3&MF3_ISMONSTER || thing->IsKindOf(RUNTIME_CLASS(AInventory))) && (thing->flags&MF_ICECORPSE || !(thing->flags&MF_CORPSE));
+	bool clipthing = (thing->player || thing->flags3&MF3_ISMONSTER || thing->IsKindOf(NAME_Inventory)) && (thing->flags&MF_ICECORPSE || !(thing->flags&MF_CORPSE));
 	bool smarterclip = !clipthing && gl_spriteclip == 3;
 	if (clipthing || gl_spriteclip > 1)
 	{
@@ -670,7 +670,7 @@ void GLSprite::Process(HWDrawInfo *di, AActor* thing, sector_t * sector, area_t 
 		if (di->FixedColormap == CM_LITE)
 		{
 			if (gl_enhanced_nightvision &&
-				(thing->IsKindOf(RUNTIME_CLASS(AInventory)) || thing->flags3&MF3_ISMONSTER || thing->flags&MF_MISSILE || thing->flags&MF_CORPSE))
+				(thing->IsKindOf(NAME_Inventory) || thing->flags3&MF3_ISMONSTER || thing->flags&MF_MISSILE || thing->flags&MF_CORPSE))
 			{
 				RenderStyle.Flags |= STYLEF_InvertSource;
 			}
