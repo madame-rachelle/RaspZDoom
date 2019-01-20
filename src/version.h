94563ff83153000b02399423d21faecb2bb0e70f
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-01-20 11:43:49 +0100

    - version is 3.7.2.

--- src/version.h
+++ src/version.h
@@ -41,16 +41,16 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "g3.7.1_legacy"
+#define VERSIONSTR "g3.7.2_legacy"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 3,7,1,0
-#define RC_PRODUCTVERSION 3,7,1,0
+#define RC_FILEVERSION 3,7,2,0
+#define RC_PRODUCTVERSION 3,7,2,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 3
 #define VER_MINOR 7
-#define VER_REVISION 1
+#define VER_REVISION 2
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
